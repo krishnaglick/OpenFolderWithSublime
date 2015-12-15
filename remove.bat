5e6b54a7f41d43baf8733c82b7ee15c92e405046
Krishna Glick <krishna.glick@ascentis.com>
2015-12-15 12:39:26 -0500

    First

---
+++ remove.bat
@@ -0,0 +1,7 @@
+@echo off
+@reg delete "HKEY_CLASSES_ROOT\*\shell\Open with Sublime Text"
+@reg delete "HKEY_CLASSES_ROOT\*\shell\Open with Sublime Text"
+@reg delete "HKEY_CLASSES_ROOT\*\shell\Open with Sublime Text\command"
+@reg delete "HKEY_CLASSES_ROOT\Folder\shell\Open with Sublime Text"
+@reg delete "HKEY_CLASSES_ROOT\Folder\shell\Open with Sublime Text"
+@reg delete "HKEY_CLASSES_ROOT\Folder\shell\Open with Sublime Text\command"
